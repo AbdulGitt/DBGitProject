8c83cb9f0f5fba1c02cea65558c60b2803c0f0eb
AbdulGitt <greenstechnology12345@gmail.com>
2024-06-24 14:26:14 +0530

    DB Project

---
+++ DjangoMySqlProject/AuthenticationApp/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class User(AbstractUser):
+    name=models.CharField(max_length=120)
+    email=models.CharField(max_length=120,unique=True)
+    password = models.CharField(max_length=120)
+    username = None
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+
+
+
+
